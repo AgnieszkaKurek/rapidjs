f30bf827c6879cb2e0b164d6410b731b879c1a03
Agnieszka Kurek <agniechakurek@gmail.com>
2016-05-05 13:45:11 +0200

    creating simple example loop "do while"

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var lineItemCount = 3;
 var currentItem = 0;
-while (currentItem <lineItemCount) {
+
+do {
     console.log("item: " + currentItem);
     currentItem++;
-}
+} while (currentItem < lineItemCount);
