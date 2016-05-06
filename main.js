e8950189cdb227475426df9b811f61781264b129
Agnieszka Kurek <agniechakurek@gmail.com>
2016-05-06 14:43:54 +0200

    crdating function with global scope

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-var lineItemCount = 5;
-
-outerLoop:
-for (var i = 0; i < lineItemCount; i++) {
-    for (var j = 0; j < 3; j++) {
-        if (j == 1)
-        continue outerLoop;
-    }
-    console.log(i);
+var description = 'hardware product' ;
+function updateProduct() {
+    description = 'update product';
 }
+
+updateProduct();
+console.log(description);
