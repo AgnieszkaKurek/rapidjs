ec64f83821648a93e7e685aa4b28a7ba75a12ac2
Agnieszka Kurek <agniechakurek@gmail.com>
2016-05-09 12:47:58 +0200

    used bitwise operators

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var level = 5;
-var base = 10;
-var nextLevel = level-- * base;
-console.log(nextLevel);
+var  num1 = parseInt ('1010', 2);
+var  num2 = parseInt ('0110', 2);
+var total = num1 & num2;
+console.log(total.toString(2));
