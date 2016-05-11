51cb2dd0ebcfb9960e855fd68a451fe0d777326b
Agnieszka Kurek <agniechakurek@gmail.com>
2016-05-11 19:10:43 +0200

    created example object using of descriptor

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 'use strict';
-var task = { 
-   _dueDate: '1/15/16'
-};
-Object.defineProperty(task, 'dueDate', {
-    get: function () {
-        return this._dueDate;
-    },
-    set: function (newValue) {
-        this._dueDate = newValue;
-    }
+var task = { };
+  Object.defineProperties(task, {
+   'text':{
+       value:'New Task'
+   }
+  });
   
-});
-task.dueDate = '2/2/16';
-console.log(task.dueDate);
+var descriptor = Object.getOwnPropertyDescriptor(task, 'text');
+console.log(descriptor);
